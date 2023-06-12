7b8a2e9038c3936bd850a37ec9148f2a16cff3f9
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-06-11 21:59:12 -0300

    WIP: starts wordlist implementation

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,13 @@
 #include <iostream> /*lib de I/O*/
 #include <cstdlib>
+#include <algorithm>
+#include <iomanip>
+#include <fstream>
 #include <string>
+#include "./utils/fileUtil.cpp"
 
 using namespace std;
-
+using namespace Utils;
 
 
 void println(string value) { 
@@ -13,40 +17,82 @@ void println(string value) {
 namespace Game {
     class HangmanGame {
         public:
-            string current_guess = "";
+            char current_guess;
             string attempted_letters = "";
             string guessed_letters = "";
+            FileUtil words;
             bool isHanged = false;
+            bool debug = true;
             bool won = false;
-            int guesses = 0;
+            int lives = 9;   /*Head, Two eyes, mouth, body, 2 arms & two legs*/
 
-            string secret_word = "MELON";
+            string secret_word = "melon";
             void play() {
-                println("Hello world - HangmanGame " + secret_word);
+                println("\n*****************************");
+                println("*  Welcome to Hangman Game  *");
+                println("*****************************");
+                words.get_content("./src/data/wordlist.txt");
+                if(debug) {
+                    println("[DEBUG] Secret word: " + secret_word);
+                }
                 while (!won && !isHanged) {
                     guess();
+                }
+                if (won) {
+                    println("\n**************");
+                    println("*  WINNER!!  *");
+                    println("**************");
+                    cout << "You won! The secret word was: " + secret_word +  "\nyou remained with " << lives << " lives. \nCongratulations!" << endl;
+                } else {
+                    println("\n***************");
+                    println("*  GAME OVER  *");
+                    println("***************");
+                    println("Oh no! You lose! The secret word was: " + secret_word + ". Try again?");
                 }
             }
         private:
             void guess() {
-                cout << "Guess a letter: ";
+                cout << "\nLives: " << lives << " - Guessed Right: " << guessed_letters << " - Attempted: " << attempted_letters << endl;
+                cout << "\nGuess a letter: ";
                 cin >> current_guess;
-                if (current_guess.length() != 1) {
-                    println("Please guess a single letter");
-                    return;
-                }
-                if (attempted_letters.find(current_guess) != string::npos) {
-                    println("You already guessed that letter");
-                    return;
-                }
+                check_already_attempted_letters();
+            }
+            void validate_guessed_letters() {
                 if (secret_word.find(current_guess) != string::npos) {
                     guessed_letters += current_guess;
-                    println("You guessed a letter");
+                    println("You guessed it right!");
+                    attempted_letters += current_guess;
                 } else {
-                    guesses++;
-                    println("You guessed a wrong letter");
+                    println("Wrong letter, try again!");
+                    attempted_letters += current_guess;
+                    lives--;
                 }
-                attempted_letters += current_guess;
+                check_game_status();
             }
-    };
+            void check_already_attempted_letters() {
+                if (attempted_letters.find(current_guess) != string::npos) {
+                    println("You already guessed that letter");
+                } else {
+                    validate_guessed_letters();
+                }
+            }
+            
+            bool contains_all_characters( const std::string &str, const std::string &prt )
+            {
+                for ( char c : prt )
+                {
+                    if ( str.find( c ) == std::string::npos )
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            void check_game_status() {
+                won = contains_all_characters(guessed_letters, secret_word);
+                isHanged = lives == 0;
+            }
+    }; 
 }
---
+++ src/utils/fileUtil.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+namespace Utils {
+    class FileUtil {
+        public:
+            ifstream inFile;
+            vector<string> words{};
+            string x;
+            vector<string> get_content(string filename) {
+                vector<string> content = read(filename);
+                return content;
+            }
+        private:
+            vector<string> read(string filename) {
+                inFile.open(filename);
+                if (!inFile) {
+                    cout << "Unable to open file";
+                    exit(1); // terminate with error
+                }
+                
+                while (inFile >> x) {
+                    words.push_back(x);
+                }
+                for (int i = 0; i < words.size(); i++) {
+                    cout << words[i] << endl;
+                }
+                return words;
+                inFile.close();
+            }
+    };
+}
+
